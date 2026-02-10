8d8a4ec8f7e33e7137e0444668c4f281e1881fc8
Rajiv Mordani <rajiv.mordani@broadcom.com>
2026-02-09 18:44:21 -0800

    Enable SSL for the development server and ignore PEM files

--- run.py
+++ run.py
@@ -5,4 +5,5 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5050)
+    app.run(debug=True, host='0.0.0.0', port=8080,
+            ssl_context=('cert.pem', 'key.pem'))
